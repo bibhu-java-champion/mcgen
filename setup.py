f01cf9a2e70b2538547cbef17816cb7af6d8ebfb
Bibhu <bibhumm83@gmail.com>
2024-02-28 02:42:52 +0530

    File Changes

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Bibhu  Mishra',
+    author_email='mbibhumm83@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
